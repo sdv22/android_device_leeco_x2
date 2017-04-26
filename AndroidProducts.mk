d2843e013113d719a741126bf30d9f3022aa3286
1nfrag <sil3ntcll@gmail.com>
2017-04-26 09:56:51 +0800

    DU-ify

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device.mk \
+    $(LOCAL_DIR)/du_x2.mk
--- du.mk
+++ du.mk
@@ -20,9 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/leeco/x2/device.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_x2
+PRODUCT_NAME := du_x2
 PRODUCT_DEVICE := x2
 PRODUCT_MANUFACTURER := LeMobile
 PRODUCT_BRAND := LeEco
