c16d04fef6999462dd0eba04d384e02cf30fd013
1nfrag <sil3ntcll@gmail.com>
2017-04-29 16:47:08 +0800

    bluetooth

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,12 +21,20 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+
+#include <cutils/properties.h>
+#include <string.h>
+
 #define BTM_DEF_LOCAL_NAME   "Le Max 2"
 // Disables read remote device feature
-#define MAX_ACL_CONNECTIONS   16
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_ACL_CONNECTIONS    7
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 // skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BT_CLEAN_TURN_ON_DISABLED 1
+#define BTM_LE_SECURE_CONN  TRUE
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #endif
